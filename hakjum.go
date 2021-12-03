60fe364e2576d8fe752a09433ba25d83622d978c
ming <88813537+euphratesriver0216@users.noreply.github.com>
2021-12-03 14:15:28 +0900

    Add files via upload

---
+++ hakjum.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var math, english, program, club, avg, sum int
+	var result string
+
+	fmt.Println("평균학점계산기 입니다.")
+	fmt.Println("수학, 영어, 프로그램, 클럽활동 순으로 학점을 입력해주세요")
+	fmt.Scan(&math, &english, &program, &club)
+
+	sum = math + english + program + club
+	avg = sum / 4
+
+	if avg >= 90 {
+		result = "A"
+	} else if avg >= 80 {
+		result = "B"
+	} else if avg >= 70 {
+		result = "C"
+	} else {
+		result = "D 70점 이하의 과목은 재수강하세요"
+	}
+	fmt.Println("학점", result, "평균", avg)
+}
