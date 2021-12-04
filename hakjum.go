dae92a66fb70d6dfa0dc44ac01a2ddfbb1f6c979
ming <88813537+euphratesriver0216@users.noreply.github.com>
2021-12-05 01:30:11 +0900

    학점 수정

--- hakjum.go
+++ hakjum.go
@@ -7,13 +7,15 @@ func main() {
 	var result string
 
 	fmt.Println("평균학점계산기 입니다.")
-	fmt.Println("수학, 영어, 프로그램, 클럽활동 순으로 학점을 입력해주세요")
+	fmt.Println("수학, 영어, 프로그램, 클럽활동 순으로 학점을 입력해주세요 (100점 만점)")
 	fmt.Scan(&math, &english, &program, &club)
 
 	sum = math + english + program + club
 	avg = sum / 4
 
-	if avg >= 90 {
+	if avg >= 101 {
+		result = "정확한 점수를 입력해주세요"
+	} else if avg >= 90 {
 		result = "A"
 	} else if avg >= 80 {
 		result = "B"
