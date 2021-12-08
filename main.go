4573a6e6c8c18d4874580ef7ae96612b0800bed5
ming <88813537+euphratesriver0216@users.noreply.github.com>
2021-12-08 23:38:01 +0900

    Add files via upload

--- main.go
+++ main.go
@@ -1,238 +1,49 @@
 package main
 
-import (
-	"context"
-	"encoding/json"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"strings"
-	"time"
-
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
-	"github.com/thedevsaddam/renderer"
-	mgo "gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
-)
-
-var rnd *renderer.Render
-var db *mgo.Database
-
-const (
-	hostName       string = "localhost:27017"
-	dbName         string = "demo_todo"
-	collectionName string = "todo"
-	port           string = ":5000"
-)
-
-type (
-	todoModel struct {
-		ID        bson.ObjectId `bson:"_id,omitempty"`
-		Title     string        `bson:"title"`
-		Completed bool          `bson:"completed"`
-		CreatedAt time.Time     `bson:"createAt"`
+func cap1(){
+	f := make([]func(), 3) //함수 리터럴 3개를 가진 슬라이스
+	fmt.Println("캡1")
+	for i := 0; i 3; i++ {
+		f[i] = func(){
+			fmt.Println(i)
+		}
 	}
+	for i :=0; i < 3; i++{
+		v := i //v 변수에 i값복사
 
-	todo struct {
-		ID        string    `json:"id"`
-		Title     string    `json:"title"`
-		Completed bool      `json:"completed"`
-		CreatedAt time.Time `json:"created_at"`
+		f[i] = func(){
+			fmt.Println(v)
+		}
 	}
-)
-
-func init() {
-	rnd = renderer.New()
-	sess, err := mgo.Dial(hostName)
-	checkErr(err)
-	sess.SetMode(mgo.Monotonic, true)
-	db = sess.DB(dbName)
+	for i := 0; i < 3; i++{
+		f[i]()
+	}
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	err := rnd.Template(w, http.StatusOK, []string{"static/home.tpl"}, nil)
-	checkErr(err)
-}
-
-func createTodo(w http.ResponseWriter, r *http.Request) {
-	var t todo
-
-	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
-		rnd.JSON(w, http.StatusProcessing, err)
-		return
+func cap2(){
+	f := make([]func(), 3) //함수 리터럴 3개를 가진 슬라이스
+	fmt.Println("캡2")
+	for i := 0; i 3; i++ {
+		f[i] = func(){
+			fmt.Println(i)
+		}
 	}
+	for i :=0; i < 3; i++{
+		v := i //v 변수에 i값복사
 
-	// simple validation
-	if t.Title == "" {
-		rnd.JSON(w, http.StatusBadRequest, renderer.M{
-			"message": "The title field is requried",
-		})
-		return
+		f[i] = func(){
+			fmt.Println(v)
+		}
 	}
-
-	// if input is okay, create a todo
-	tm := todoModel{
-		ID:        bson.NewObjectId(),
-		Title:     t.Title,
-		Completed: false,
-		CreatedAt: time.Now(),
+	for i := 0; i < 3; i++{
+		f[i]()
 	}
-	if err := db.C(collectionName).Insert(&tm); err != nil {
-		rnd.JSON(w, http.StatusProcessing, renderer.M{
-			"message": "Failed to save todo",
-			"error":   err,
-		})
-		return
-	}
-
-	rnd.JSON(w, http.StatusCreated, renderer.M{
-		"message": "Todo created successfully",
-		"todo_id": tm.ID.Hex(),
-	})
-}
-
-func updateTodo(w http.ResponseWriter, r *http.Request) {
-	id := strings.TrimSpace(chi.URLParam(r, "id"))
-
-	if !bson.IsObjectIdHex(id) {
-		rnd.JSON(w, http.StatusBadRequest, renderer.M{
-			"message": "The id is invalid",
-		})
-		return
-	}
-
-	var t todo
-
-	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
-		rnd.JSON(w, http.StatusProcessing, err)
-		return
-	}
-
-	// simple validation
-	if t.Title == "" {
-		rnd.JSON(w, http.StatusBadRequest, renderer.M{
-			"message": "The title field is requried",
-		})
-		return
-	}
-
-	// if input is okay, update a todo
-	if err := db.C(collectionName).
-		Update(
-			bson.M{"_id": bson.ObjectIdHex(id)},
-			bson.M{"title": t.Title, "completed": t.Completed},
-		); err != nil {
-		rnd.JSON(w, http.StatusProcessing, renderer.M{
-			"message": "Failed to update todo",
-			"error":   err,
-		})
-		return
-	}
-
-	rnd.JSON(w, http.StatusOK, renderer.M{
-		"message": "Todo updated successfully",
-	})
-}
-
-func fetchTodos(w http.ResponseWriter, r *http.Request) {
-	todos := []todoModel{}
-
-	if err := db.C(collectionName).
-		Find(bson.M{}).
-		All(&todos); err != nil {
-		rnd.JSON(w, http.StatusProcessing, renderer.M{
-			"message": "Failed to fetch todo",
-			"error":   err,
-		})
-		return
-	}
-
-	todoList := []todo{}
-	for _, t := range todos {
-		todoList = append(todoList, todo{
-			ID:        t.ID.Hex(),
-			Title:     t.Title,
-			Completed: t.Completed,
-			CreatedAt: t.CreatedAt,
-		})
-	}
-
-	rnd.JSON(w, http.StatusOK, renderer.M{
-		"data": todoList,
-	})
-}
-
-func deleteTodo(w http.ResponseWriter, r *http.Request) {
-	id := strings.TrimSpace(chi.URLParam(r, "id"))
-
-	if !bson.IsObjectIdHex(id) {
-		rnd.JSON(w, http.StatusBadRequest, renderer.M{
-			"message": "The id is invalid",
-		})
-		return
-	}
-
-	if err := db.C(collectionName).RemoveId(bson.ObjectIdHex(id)); err != nil {
-		rnd.JSON(w, http.StatusProcessing, renderer.M{
-			"message": "Failed to delete todo",
-			"error":   err,
-		})
-		return
-	}
-
-	rnd.JSON(w, http.StatusOK, renderer.M{
-		"message": "Todo deleted successfully",
-	})
 }
 
 func main() {
-	stopChan := make(chan os.Signal)
-	signal.Notify(stopChan, os.Interrupt)
+	//함수 리터럴 외부변수를 내부로 가져오는 것을 캡쳐
+	// 캡쳐는 값복사가 아니라 래퍼런스 (참조)형태로 가져온다.
+	// 변수의 주소를 포인터 값으로 복사한다.
 
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", homeHandler)
 
-	r.Mount("/todo", todoHandlers())
-
-	srv := &http.Server{
-		Addr:         port,
-		Handler:      r,
-		ReadTimeout:  60 * time.Second,
-		WriteTimeout: 60 * time.Second,
-		IdleTimeout:  60 * time.Second,
-	}
-
-	go func() {
-		log.Println("Listening on port ", port)
-		if err := srv.ListenAndServe(); err != nil {
-			log.Printf("listen: %s\n", err)
-		}
-	}()
-
-	<-stopChan
-	log.Println("Shutting down server...")
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	srv.Shutdown(ctx)
-	defer cancel()
-	log.Println("Server gracefully stopped!")
-}
-
-func todoHandlers() http.Handler {
-	rg := chi.NewRouter()
-	rg.Group(func(r chi.Router) {
-		r.Get("/", fetchTodos)
-		r.Post("/", createTodo)
-		r.Put("/{id}", updateTodo)
-		r.Delete("/{id}", deleteTodo)
-	})
-	return rg
-}
-
-func checkErr(err error) {
-	if err != nil {
-		log.Fatal(err) //respond with error page or message
-	}
 }
